ab663b1e60f7b8edefb80a7d6fc9bba9598137a4
AgohChris <agihchris90@gmail.com>
2025-06-14 07:18:41 +0000

    config

---
+++ noviscoworking/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import *
+from .serializers import *
+
+# Create your views here.
+
+
+def HomeView(request):
+    return render(request, 'home.html', {})
